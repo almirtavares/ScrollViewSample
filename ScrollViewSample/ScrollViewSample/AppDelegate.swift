a25bc694d109cebd84e56a4e1b2d29dbdf3f75df
Almir <eng.almirtavares@gmail.com>
2020-08-04 13:58:39 -0300

    ScrollView
    ScrollView Sample

---
+++ ScrollViewSample/ScrollViewSample/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ScrollViewSample
+//
+//  Created by Almir Tavares on 04/08/20.
+//  Copyright © 2020 Dev Venture. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ ScrollViewSample/ScrollViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewSample
+//
+//  Created by Almir Tavares on 04/08/20.
+//  Copyright © 2020 Dev Venture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
